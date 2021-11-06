3bf6d7155e9135de532ff819a4c7d27944a3c5e9
Ивкин Станислав <109stas@gmail.com>
2021-11-06 10:58:21 +0300

    Getting url from open chrome

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
-
-use std::{thread, time::Duration};
-use windows::{
-    Win32::Foundation::PWSTR,
-    Win32::UI::WindowsAndMessaging::{
-        GetForegroundWindow,
-        GetWindowTextLengthW,
-        GetWindowTextW,
+use core::ops::DerefMut;
+use std::{
+    thread,
+    time::Duration
+};
+use windows::Win32::{
+    Foundation::*,
+    System::Com::*,
+    System::{
+        OleAutomation::*,
+        PropertiesSystem::*,
+    },
+    UI::{
+        WindowsAndMessaging::*,
+        Accessibility::*,
     },
 };
 use anyhow::{
     Result,
     anyhow,
 };
+use widestring::U16CStr;
 
+// error: process didn't exit successfully: `target\debug\window-title-getter.exe` (exit code: 0xc0000374, STATUS_HEAP_CORRUPTION)
 fn main() {
     loop {
         unsafe {
@@ -21,10 +30,17 @@ fn main() {
                     println!("Window title: {}", title);
                 }
                 Err(error) => {
-                    println!("Error: {}", error);
+                    println!("Error getting window title: {}", error);
                 }
             }
-            
+            match get_active_browser_url() {
+                Ok(url) => {
+                    println!("Browser URL: {}", url);
+                }
+                Err(error) => {
+                    println!("Error getting browser URL: {}", error);
+                }
+            }
         }
         thread::sleep(Duration::from_secs(1));
     }
@@ -53,4 +69,35 @@ unsafe fn get_active_window_title() -> Result<String> {
     else {
         Err(anyhow!("Foreground window hwnd is invalid: {}", foreground_window_hwnd.0))
     }
+}
+
+unsafe fn get_active_browser_url() -> Result<String> {
+    let foreground_window_hwnd = GetForegroundWindow();
+    CoInitializeEx(std::ptr::null(), COINIT_MULTITHREADED)?;
+    let ui_automation: IUIAutomation = CoCreateInstance(&CUIAutomation, None, CLSCTX_INPROC_SERVER)?;
+    let ui_element = ui_automation.ElementFromHandle(foreground_window_hwnd)?;
+    get_chrome_url(ui_automation, ui_element)
+}
+
+unsafe fn get_chrome_url(ui_automation: IUIAutomation, ui_element: IUIAutomationElement) -> Result<String> {
+    let search_condition = create_search_condition_for_url_editor(ui_automation)?;
+    let edit_element = ui_element.FindFirst(TreeScope_Descendants, search_condition)?;
+    get_value_from_ui_element(edit_element)
+}
+
+unsafe fn create_search_condition_for_url_editor(ui_automation: IUIAutomation) -> Result<IUIAutomationCondition> {
+    const CHROME_CONTROL_ID: i32 = 0xC354;
+    let mut variant = VARIANT::default();
+    variant.Anonymous.Anonymous.deref_mut().vt = VT_I4.0 as u16;
+    variant.Anonymous.Anonymous.deref_mut().Anonymous.lVal = CHROME_CONTROL_ID;
+
+    let search_condition = ui_automation.CreatePropertyCondition(UIA_ControlTypePropertyId, variant)?;
+    Ok(search_condition)
+}
+
+unsafe fn get_value_from_ui_element(ui_element: IUIAutomationElement) -> Result<String> {
+    let value = ui_element.GetCurrentPropertyValue(UIA_ValueValuePropertyId)?;
+    let title = VariantToStringAlloc(&value)?;
+    let title = U16CStr::from_ptr_str(title.0).to_string()?;
+    Ok(title)
 }
